87c6c7579f020429e53692488d41a0a6ddb76ac1
christiankreuzberger <christian.kreuzberger@itec.uni-klu.ac.at>
2014-03-10 13:58:28 +0100

    added a "static" congestion window that only increases until we meet the receiver max window, and used it in window downloader.

---
+++ extensions/utils/staticcongestionwindow.cc
@@ -0,0 +1,28 @@
+#include "staticcongestionwindow.h"
+
+
+using namespace ns3;
+
+StaticCongestionWindow::StaticCongestionWindow() : CongestionWindow()
+{
+}
+
+
+int StaticCongestionWindow::IncreaseWindow()
+{
+  this->window_size = this->window_size * 2;
+
+  if (this->window_size > this->recv_window_size)
+    this->window_size = this->recv_window_size;
+
+  // do nothing
+  return this->window_size;
+}
+
+
+int StaticCongestionWindow::DecreaseWindow()
+{
+
+  // do nothing
+  return this->window_size;
+}
