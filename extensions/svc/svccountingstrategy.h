da447dd67c4ba20b9aea57dd0a50762f54f0fbbd
christiankreuzberger <christian.kreuzberger@itec.uni-klu.ac.at>
2014-04-14 11:02:42 +0200

    rewriteing svc counting strategy to use the new classes

--- extensions/svc/svccountingstrategy.h
+++ extensions/svc/svccountingstrategy.h
@@ -35,13 +35,30 @@
 
 #include "svcleveltag.h"
 #include "svcbitratetag.h"
+#include "facepacketstatistic.h"
+
 #include "../utils/deadlinetag.h"
+// include idownloader interface for PACKET_SIZE and OVERHEAD
+#include "../utils/idownloader.h"
+
+#include "../droppingPolicy/smoothlevelstatistics.h"
+#include "../droppingPolicy/packetbaseddroppingpolicy.h"
+
+// include boost libraries for map
+#include <boost/unordered_map.hpp>
+#include <boost/foreach.hpp>
 
 
 #include <stdio.h>
 
 
 #define UPDATE_ALPHA 0.2
+#define NUM_LEVELS   3
+#define RESET_STATISTICS_TIMER 1.0
+
+// set the default content level (default: 0) - set to 1 if you want to punish non svc content
+#define DEFAULT_CONTENT_LEVEL 0
+
 
 namespace ns3 {
 namespace ndn {
@@ -60,91 +77,44 @@ public:
 
   SVCCountingStrategy () : super() {
     fprintf(stderr, "In constructor...\n");
-    levelCount.reserve(3);
-    levelProb.reserve(3);
-    dropProb.reserve(3);
-    levelCount[0] = levelCount[1] = levelCount[2] = 0;
-    levelProb[0] = levelProb[1] = levelProb[2] = 0.0;
-    dropProb[0] = dropProb[1] = dropProb[2] = 0.0;
-    resetLevelCount();
+    // init the uniform random variable
+    randomNumber = UniformVariable(0,1);
+
+    // init amount of levels
+    //stats.CreateLevels(NUM_LEVELS);
+
+    // schedule first reset event
+    ResetStatisticsTimer = Simulator::Schedule(Seconds(RESET_STATISTICS_TIMER), &SVCCountingStrategy::resetLevelCount, this);
   }
 
   virtual void OnInterest(Ptr< Face > inface, Ptr< Interest > interest);
+  virtual void AddFace(Ptr<Face> face);
+  virtual void RemoveFace(Ptr<Face> face);
+
+
+  static uint64_t getPhysicalBitrate(Ptr<Face> face);
 
 protected:
-  std::vector<unsigned int> levelCount;
-  std::vector<double> levelProb;
-  std::vector<double> dropProb;
+  void resetLevelCount();
 
-  unsigned int last_packets_per_second;
+  UniformVariable randomNumber;
 
-  void resetLevelCount() {
+  /* vector to store all faces */
+  std::vector<Ptr<ndn::Face> > faces;
 
-    fprintf(stderr, "CountingStrategy: LevelCounts: %d, %d, %d\n", levelCount[0], levelCount[1], levelCount[2]);
+  /* map for storing stats for all faces */
+  typedef boost::unordered_map
+    <
+      int/*faceID*/,
+      svc::FacePacketStatistic*
+    > FacePacketStatisticMap;
 
-    last_packets_per_second = 0;
+  FacePacketStatisticMap map;
 
-
-    for (int i = 0; i < 3; i++)
-    {
-      last_packets_per_second += levelCount[i];
-    }
-
-    fprintf(stderr, "last_packets_per_second=%d\n", last_packets_per_second);
-
-
-    double congestionPercentage = (double)(last_packets_per_second-110) / ((double)last_packets_per_second);
-
-
-
-    if (last_packets_per_second != 0)
-    {
-
-      for (int i = 0; i < 3; i++)
-      {
-
-        levelProb[i] = levelProb[i] * (1-UPDATE_ALPHA) +
-            (double)levelCount[i]/(double)last_packets_per_second * UPDATE_ALPHA;
-      }
-
-      dropProb[0] = dropProb[1] = dropProb[2] = 0.0;
-
-      if (congestionPercentage > 0.0)
-      {
-        // calculate congestion drop percentage
-        if (levelProb[2] > 0.0)
-          dropProb[2] = congestionPercentage / levelProb[2];
-
-        if (dropProb[2] > 1.0)
-        {
-          dropProb[2] = 1.0;
-
-          if (levelProb[1] > 0.0)
-            dropProb[1] = (congestionPercentage - levelProb[2]*dropProb[2]) / levelProb[1];
-        }
-      }
-
-      fprintf(stderr, "CountingStrategy: Cong=%f, DropProb[2]=%f, DropProb[1]=%f\n", congestionPercentage, dropProb[2], dropProb[1]);
-
-
-
-
-    }
-
-    fprintf(stderr, "CountingStrategy: Probabilities: %f, %f, %f\n", levelProb[0], levelProb[1], levelProb[2]);
-
-
-    levelCount.clear(); levelCount.reserve(3);
-    levelCount[0] = levelCount[1] = levelCount[2] = 0;
-
-    ResetStatisticsTimer = Simulator::Schedule(Seconds(1.0), &SVCCountingStrategy::resetLevelCount, this);
-  }
 
   static LogComponent g_log;
 
   EventId ResetStatisticsTimer;
-
-
 };
 
 template<class Parent>
@@ -167,37 +137,136 @@ std::string SVCCountingStrategy<Parent>::GetLogName ()
 }
 
 
+template<class Parent>
+uint64_t SVCCountingStrategy<Parent>::getPhysicalBitrate(Ptr<Face> face)
+{
+  // Get Device Bitrate
+  Ptr<PointToPointNetDevice> nd1 =
+      face->GetNode()->GetDevice(0)->GetObject<PointToPointNetDevice>();
+  DataRateValue dv;
+  nd1->GetAttribute("DataRate", dv);
+  DataRate d = dv.Get();
+  return d.GetBitRate();
+}
+
+
+template<class Parent>
+void SVCCountingStrategy<Parent>::resetLevelCount() {
+  for(std::vector<Ptr<ndn::Face> >::iterator it = faces.begin ();
+      it !=  faces.end (); ++it)
+  {
+    // get face pointer
+    Ptr<Face> face = *it;
+
+    // calculate max_packets and metric
+    uint64_t bitrate = getPhysicalBitrate(face);
+    int max_packets = bitrate / ( MAX_PACKET_PAYLOAD + PACKET_OVERHEAD ) / 8;
+    double metric = 0.0;
+
+    // get last packets
+    unsigned int last_packets_ps = this->map[face->GetId ()]->GetPacketsPerTime ();
+
+    // calculate metric only if last_packets > max_packets, else metric stays 0.0
+    if (last_packets_ps > max_packets)
+      metric = ((double)(last_packets_ps-max_packets)) / (double)(last_packets_ps);
+
+
+    // refresh statistics, update the policy (= feed policy and then reset stats )
+    this->map[face->GetId ()]->UpdatePolicy ();
+
+    // reset packets per second
+    this->map[face->GetId ()]->SetPacketsPerTime (0);
+  }
+
+
+  // schedule next reset event
+  ResetStatisticsTimer = Simulator::Schedule(Seconds(RESET_STATISTICS_TIMER), &SVCCountingStrategy::resetLevelCount, this);
+}
+
+
+/* add face */
+template<class Parent>
+void SVCCountingStrategy<Parent>::AddFace (Ptr<Face> face)
+{
+  // add face to our map
+  map[face->GetId ()] = new svc::FacePacketStatistic();
+  // tell the stats how many levels we will have
+  map[face->GetId ()]->getStats()->CreateLevels(NUM_LEVELS);
+
+  // add face to faces vector
+  faces.push_back (face);
+
+  super::AddFace(face);
+}
+
+
+/* remove face */
+template<class Parent>
+void SVCCountingStrategy<Parent>::RemoveFace (Ptr<Face> face)
+{
+  // first: get pointer of the Statistics object
+  svc::FacePacketStatistic* p = map[face->GetId ()];
+
+  // second: erase face from map
+  map.erase (face->GetId ());
+
+  // third: if not already happened, delete the Statistics object
+  if (p != NULL)
+    delete p;
+
+
+  // remove face from faces vector
+  for (std::vector<Ptr<ndn::Face> >::iterator it = faces.begin ();
+      it !=  faces.end (); ++it)
+  {
+    if (face== (*it))//todo
+    {
+      faces.erase (it);
+      break;
+    }
+  }
+
+  super::RemoveFace (face);
+}
+
+
 
 template<class Parent>
 void SVCCountingStrategy<Parent>::OnInterest (Ptr< Face > inface, Ptr< Interest > interest)
 {
-  Ptr<Packet> packet = Wire::FromInterest(interest);
-  SVCLevelTag levelTag;
-  DeadlineTag deadlineTag;
+  // get the actual packet so we can access tags
+  Ptr<Packet> packet = Wire::FromInterest (interest);
+
+
+  /* DeadlineTag deadlineTag;
 
   uint64_t deadline = 0;
 
-  bool deadlineTagExists = packet->PeekPacketTag(deadlineTag);
+  bool deadlineTagExists = packet->PeekPacketTag (deadlineTag);
   if (deadlineTagExists)
   {
     //fprintf(stderr, "deadline tag found!\n");
-    deadline = deadlineTag.Get();
+    deadline = deadlineTag.Get ();
   }
 
   if (deadline == 0)
   {
     deadline = 1000;
-  }
+  } */
 
 
+  // STEP 0: Check if duplicate request
 
-  //check if enchancment layer
-  bool tagExists = packet->PeekPacketTag(levelTag);
 
-  int level = 0;
-  if (tagExists)
+  // STEP 1: check if SVC base layer or enhancement layer
+  // STEP 1a: map SVC Layer to a layer between 0 and 2
+  SVCLevelTag levelTag;
+  bool svcLevelTagExists = packet->PeekPacketTag (levelTag);
+
+  int level = DEFAULT_CONTENT_LEVEL;
+  if (svcLevelTagExists)
   {
-    level = levelTag.Get();
+    level = levelTag.Get ();
   }
   if (level == 16)
     level = 1;
@@ -206,19 +275,22 @@ void SVCCountingStrategy<Parent>::OnInterest (Ptr< Face > inface, Ptr< Interest 
   else
     level = 0;
 
-  levelCount[level] = levelCount[level] + 1;
+
+
+  // STEP 1: Check if inface can handle the return packet
+
+  // increase level counter for that face
+  this->map[inface->GetId ()]->IncreasePackets (level);
 
   // check if RandomNumber(0,1) < DropProbability
   // if yes --> drop
   // draw a random number
-  UniformVariable rand (0,1);
+  double val = this->map[inface->GetId ()]->GetDropProbability (level);
 
-  double val = rand.GetValue();
-
-  if (val < dropProb[level])
+  if (randomNumber.GetValue() < val)
   {
     // DROP
-    //NS_LOG_UNCOND("Strategy: Dropping Interest " << interest->GetName ().toUri());
+    // NS_LOG_UNCOND("Strategy: Dropping Interest " << interest->GetName ().toUri());
 
     Ptr<Interest> nack = Create<Interest> (*interest);
     nack->SetNack (ndn::Interest::NACK_GIVEUP_PIT); // set this since ndn changes it anyway to this.
@@ -232,9 +304,6 @@ void SVCCountingStrategy<Parent>::OnInterest (Ptr< Face > inface, Ptr< Interest 
     return;
   }
 
-
-
-  //fprintf(stderr, "CountingStrategy: LevelCounts: %d, %d, %d\n", levelCount[0], levelCount[1], levelCount[2]);
 
   super::OnInterest(inface,interest);
 }
