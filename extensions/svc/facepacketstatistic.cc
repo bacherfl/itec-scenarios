d5c94490adace630165e09828afe227df292f918
christiankreuzberger <christian.kreuzberger@itec.uni-klu.ac.at>
2014-04-14 11:01:57 +0200

    added class FacePacketStatistics

---
+++ extensions/svc/facepacketstatistic.cc
@@ -0,0 +1,65 @@
+#include "facepacketstatistic.h"
+
+using namespace ns3::ndn::svc;
+
+NS_LOG_COMPONENT_DEFINE ("FacePacketStatistic");
+
+FacePacketStatistic::FacePacketStatistic()
+{
+  policy = new PacketBasedDroppingPolicy();
+  stats  = new SmoothLevelStatistics();
+}
+
+
+FacePacketStatistic::~FacePacketStatistic()
+{
+  delete policy;
+  delete stats;
+}
+
+
+LevelStatistics* FacePacketStatistic::getStats()
+{
+  return stats;
+}
+
+
+DroppingPolicy* FacePacketStatistic::getPolicy()
+{
+  return policy;
+}
+
+void FacePacketStatistic::UpdatePolicy()
+{
+  // Refresh statistics
+  this->stats->RefreshStatistics();
+  // update (= feed) policy
+  this->policy->Feed(*(this->stats));
+  // reset stats counters
+  this->stats->ResetCounters();
+}
+
+
+void FacePacketStatistic::IncreasePackets(unsigned int level)
+{
+  this->packets_per_time++;
+  this->stats->IncreaseLevelCounter(level);
+}
+
+
+double FacePacketStatistic::GetDropProbability(unsigned int level)
+{
+  return this->policy->GetDroppingProbability(level);
+}
+
+
+void FacePacketStatistic::SetPacketsPerTime(unsigned int packets)
+{
+  this->packets_per_time = packets;
+}
+
+
+unsigned int FacePacketStatistic::GetPacketsPerTime()
+{
+  return this->packets_per_time;
+}
---
+++ extensions/svc/facepacketstatistic.h
@@ -0,0 +1,46 @@
+#ifndef FACEPACKETSTATISTIC_H
+#define FACEPACKETSTATISTIC_H
+
+#include "ns3-dev/ns3/point-to-point-module.h"
+#include "ns3-dev/ns3/network-module.h"
+#include <ns3/log.h>
+#include <ns3/ndn-face.h>
+
+#include "../droppingPolicy/packetbaseddroppingpolicy.h"
+#include "../droppingPolicy/smoothlevelstatistics.h"
+
+
+namespace ns3
+{
+  namespace ndn
+  {
+    namespace svc
+    {
+      class FacePacketStatistic
+      {
+      public:
+        FacePacketStatistic();
+        ~FacePacketStatistic();
+        LevelStatistics* getStats();
+        DroppingPolicy*  getPolicy();
+
+        void SetPacketsPerTime(unsigned int packets);
+        unsigned int GetPacketsPerTime();
+
+        void IncreasePackets(unsigned int level);
+        double GetDropProbability(unsigned int level);
+
+
+        void UpdatePolicy();
+
+      protected:
+        unsigned int packets_per_time;
+        LevelStatistics* stats;
+        DroppingPolicy* policy;
+
+      };
+    }
+  }
+}
+
+#endif // FACEPACKETSTATISTIC_H
