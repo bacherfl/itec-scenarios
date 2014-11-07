8a4ef81b41beab5824c58e5716e2185295041883
Daniel Posch <daniel.posch@aau.at>
2014-11-07 08:20:11 +0100

    changed default parameters.

--- extensions/utils/parameterconfiguration.h
+++ extensions/utils/parameterconfiguration.h
@@ -7,19 +7,19 @@
 
 //default values can be overriden:
 
-#define P_ALPHA 0.25
+#define P_ALPHA 0.30
 
 #define P_X_DROPPING 0.4
 
-#define P_PROBING_TRAFFIC 0.15
+#define P_PROBING_TRAFFIC 0.20
 #define P_SHIFT_THRESHOLD 0.01
-#define P_SHIFT_TRAFFIC 0.20
+#define P_SHIFT_TRAFFIC 0.30
 
-#define P_UPDATE_INTERVALL 0.2
+#define P_UPDATE_INTERVALL 0.5
 #define P_MAX_LAYERS 1
 #define P_DROP_FACE_ID -1
 
-#define P_RELIABILITY_THRESHOLD 0.95
+#define P_RELIABILITY_THRESHOLD 0.70
 
 namespace ns3
 {
